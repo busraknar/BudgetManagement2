68023f06786c6aeb7fb0d4dc8414d6d56240285a
busraknar <b.ertas91@gmail.com>
2024-12-04 13:56:28 +0300

    Validators added

---
+++ BudgetManagement.Business/Validators/CategoryValidator.cs
@@ -0,0 +1,16 @@
+﻿using BudgetManagement.Entities.Models;
+using FluentValidation;
+
+namespace BudgetManagement.Business.Validators
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty()
+               .WithMessage("Kategori alanı boş geçilemez.");
+
+
+        }
+    }
+}
---
+++ BudgetManagement.Business/Validators/ExpenseValidator.cs
@@ -0,0 +1,17 @@
+﻿using BudgetManagement.Entities.Models;
+using FluentValidation;
+
+namespace BudgetManagement.Business.Validators
+{
+    public class ExpenseValidator : AbstractValidator<Expense>
+    {
+        public ExpenseValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty()
+               .WithMessage("Gider alanı boş geçilemez.");
+
+            RuleFor(p => p.Amount).NotEmpty()
+               .WithMessage("Miktar alanı boş geçilemez.");
+        }
+    }
+}
