4b099a0efafbf79b65bb57834247a8b18df272cd
busraknar <b.ertas91@gmail.com>
2024-12-08 03:05:25 +0300

    CategoryType migrations done

---
+++ BudgetManagement.DataAccess/Migrations/20241207235323_CategoryTypeNamed.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BudgetManagement.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryTypeNamed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
