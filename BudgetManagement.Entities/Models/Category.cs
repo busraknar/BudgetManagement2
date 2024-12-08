4d92db864070dd06fc0b9cc360b7df73e2cbf3f9
busraknar <b.ertas91@gmail.com>
2024-12-08 03:06:33 +0300

    CategoryType added t Category class

--- BudgetManagement.Entities/Models/Category.cs
+++ BudgetManagement.Entities/Models/Category.cs
@@ -4,5 +4,14 @@ namespace BudgetManagement.Entities.Models
 {
     public class Category : BaseEntity
     {
+        public CategoryType Type { get; set; } // Gelir veya Gider
+
     }
+    public enum CategoryType
+    {
+        Income = 1,  // Gelir kategorileri için
+        Expense = 2  // Gider kategorileri için
+    }
+
 }
+
